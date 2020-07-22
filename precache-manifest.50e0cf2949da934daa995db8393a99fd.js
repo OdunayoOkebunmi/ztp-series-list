9227d8fe5e67b5da1d72c72f5ef612746aea937d
odunayo <okebunmiodunayo@gmail.com>
2020-07-23 00:53:02 +0100

    Updates

---
+++ precache-manifest.50e0cf2949da934daa995db8393a99fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f00d48cb756bf05a97fc5911962bc9",
+    "url": "/ztp-series-list/index.html"
+  },
+  {
+    "revision": "9204f94ca66ce0fc2b56",
+    "url": "/ztp-series-list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2816da6ed3d94199f948",
+    "url": "/ztp-series-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9204f94ca66ce0fc2b56",
+    "url": "/ztp-series-list/static/js/2.0d8b1630.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ztp-series-list/static/js/2.0d8b1630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2816da6ed3d94199f948",
+    "url": "/ztp-series-list/static/js/main.c8226e98.chunk.js"
+  },
+  {
+    "revision": "213035d7261b63cbaf0d",
+    "url": "/ztp-series-list/static/js/runtime-main.ed0fee4e.js"
+  }
+]);
